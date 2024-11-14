2804cd1b910cef92005e2ccef5f1ac0d6e9d8002
martagp14 <57727166+martagp14@users.noreply.github.com>
2024-11-14 11:22:30 +0100

    More like a restart
    New project with angular and bootstrap. Added a navbar and not foung page.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutMePageComponent } from './about-me-page/about-me-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomePageComponent
+    },
+    {
+        path:'aboutMe',
+        component:AboutMePageComponent
+    },
+    {
+        path:'projects',
+        component:ProjectsPageComponent
+    },
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch:'full'
+    },
+    {
+        path:'**',
+        component: NotFoundPageComponent
+    }
+];
