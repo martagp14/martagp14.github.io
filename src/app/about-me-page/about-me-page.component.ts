883dae1e2915b6618db1150c7292f9f0ff2baf8f
martagp14 <57727166+martagp14@users.noreply.github.com>
2025-01-31 13:26:17 +0100

    Added button for downloading english CV

--- src/app/about-me-page/about-me-page.component.ts
+++ src/app/about-me-page/about-me-page.component.ts
@@ -11,11 +11,18 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class AboutMePageComponent {
 
-  downloadCV() {
-    const link = document.createElement("a");
-    link.href = "assets/CV/CV Marta García Pérez.pdf"; // Cambia esta ruta al archivo PDF
-    link.download = "CV Marta García Pérez.pdf"; // Nombre del archivo descargado
-    link.click();
+  downloadCV(english:boolean) {
+    if(english==false){
+      const link = document.createElement("a");
+      link.href = "assets/CV/CV Marta García Pérez.pdf"; // Cambia esta ruta al archivo PDF
+      link.download = "CV Marta García Pérez.pdf"; // Nombre del archivo descargado
+      link.click();
+    }else{
+      const link = document.createElement("a");
+      link.href = "assets/CV/CV Marta Garcia Perez English.pdf"; // Cambia esta ruta al archivo PDF
+      link.download = "CV Marta Garcia Perez.pdf"; // Nombre del archivo descargado
+      link.click();
+    }
   }
 
 }
